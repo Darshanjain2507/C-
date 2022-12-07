75c7157f3697509b09bfce0bacc2fcfa81f9c659
DARSHAN JAIN <108194698+Darshanjain2507@users.noreply.github.com>
2022-12-08 02:14:28 +0530

    Create delete middle element.cpp

---
+++ stack/delete middle element.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#incclude<stack>
+using namespace std;
+
+void solve(stack<int>&inputStack, int count ,int size){
+    //base case
+    
+    if(count==size/2)
+    {
+        inputStack.pop();
+        return ;
+    }    
+    int val=inputStack.top();
+      inputStack.pop();
+    
+    //recusive call
+    solve(inputStack,count+1,size);
+    inputStack.push(val);
+}
+
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+  int count=0;
+    solve(inputStack,count,N);
+   
+}
